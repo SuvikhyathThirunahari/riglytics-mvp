1b505944df2b21de59159ce65aff4ec1aad0aceb
Suvikhyath Thirunahari <suvikhyath03@gmail.com>
2026-06-14 15:26:11 +0530

    Configure production CORS

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,11 +1,29 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
 
-SAMPLE_DATA_PATH = os.path.join(
+DATA_DIR = os.path.join(
     BASE_DIR,
     "..",
-    "data",
+    "data"
+)
+
+UPLOAD_DIR = os.path.join(
+    DATA_DIR,
+    "uploads"
+)
+
+SAMPLE_DATA_PATH = os.path.join(
+    DATA_DIR,
     "sample",
     "drilling_data.csv"
+)
+
+os.makedirs(
+    UPLOAD_DIR,
+    exist_ok=True
 )
